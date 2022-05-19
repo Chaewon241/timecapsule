7b76ccf8c1a8d61999650882dfc8e3f99067d281
ggeggrgg <ggeggrgg@naver.com>
2022-05-19 19:01:44 +0900

    add MemberForm

---
+++ src/main/java/com/timecapsule/timecapsule/controller/MemberForm.java
@@ -0,0 +1,21 @@
+package com.timecapsule.timecapsule.controller;
+
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+public class MemberForm {
+
+    @NotEmpty(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotEmpty(message = "닉네임은 필수입니다.")
+    private String nickname;
+
+    @NotEmpty(message = "닉네임은 필수 입니다.")
+    private String phoneNumber;
+
+    @NotEmpty(message = "닉네임은 필수 입니다.")
+    private String password;
+}
