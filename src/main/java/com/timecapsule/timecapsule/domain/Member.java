ce7b8335f821820c6cb73b9ff98bb777a1436a8f
ggeggrgg <ggeggrgg@naver.com>
2022-05-19 19:00:40 +0900

    add member

---
+++ src/main/java/com/timecapsule/timecapsule/domain/Member.java
@@ -0,0 +1,40 @@
+package com.timecapsule.timecapsule.domain;
+
+import com.timecapsule.timecapsule.domain.dto.MemberDto;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String email;
+    private String password;
+    private String nickname;
+    private String phoneNumber;
+
+
+    @OneToMany
+    private List<GroupMember> groupMembers;
+
+    protected Member(){}
+
+    public Member(String email, String password, String phoneNumber, String nickname){
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.nickname = nickname;
+    }
+
+    public void updateMemberInfo(MemberDto dto){
+        this.email = dto.getEmail();
+        this.password = dto.getPassword();
+        this.phoneNumber = dto.getPhoneNumber();
+        this.nickname = dto.getNickName();
+    }
+}
