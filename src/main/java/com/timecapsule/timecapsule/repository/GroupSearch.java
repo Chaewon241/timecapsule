bfb16c92bb41be057716ea9ca20a52511ceab1d4
ggeggrgg <ggeggrgg@naver.com>
2022-05-25 19:53:25 +0900

    Add GroupSearch for GroupRepository

---
+++ src/main/java/com/timecapsule/timecapsule/repository/GroupSearch.java
@@ -0,0 +1,10 @@
+package com.timecapsule.timecapsule.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class GroupSearch {
+    private String LeaderName;
+    private String GroupName;
+}
