2a99b49a2a669a61f6df70cd5dcbef603c73cd02
Choi Dasom <dasomchoi52@gmail.com>
2022-05-25 19:56:15 +0900

    Fix TimecapsuleApplication to rename application

--- src/main/java/com/timecapsule/timecapsule/TimecapsuleApplication.java
+++ src/main/java/com/timecapsule/timecapsule/TimecapsuleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TimecapsuleApplication {
+public class TimeCapsuleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimecapsuleApplication.class, args);
+		SpringApplication.run(TimeCapsuleApplication.class, args);
 	}
 
 }
