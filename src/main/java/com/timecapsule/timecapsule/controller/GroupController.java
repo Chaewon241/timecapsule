79db812e16b875f351f22b2603340c694c23760e
ggeggrgg <ggeggrgg@naver.com>
2022-06-03 20:50:56 +0900

    Fix annotation

--- src/main/java/com/timecapsule/timecapsule/controller/GroupController.java
+++ src/main/java/com/timecapsule/timecapsule/controller/GroupController.java
@@ -24,19 +24,24 @@ import java.util.List;
 public class GroupController {
 
     private final GroupService groupService;
-    private final MemberService memberService;
 
-    //Get 매핑으로 group/new 로 넘어오면 그룹을 새로 생성하는 폼을 보여줍니다.
-    //그룹 생성을 위한 createGroupForm 페이지 필요
+    /**
+     * 그룹을 생성할 페이지로 넘어가게 만들어줄 매핑입니다.
+     * @return : 그룹 생성 페이지로 이동
+     */
     @GetMapping("/group/new")
     public String createForm(Model model) {
         model.addAttribute("GroupForm", new GroupForm());
-        return "/createGroupForm.html";
+        return "createGroupForm";
     }
 
-    //Post 매핑으로 group/new 로 넘어오면 폼의 데이터로 그룹을 생성함
-    //BindingResult 에 에러가 있을 시 createMemberForm 으로 다시 넘김
-    //session 에 있는 memberId 를 이용해서 현재 로그인하고 있는 회원을 그룹 leader 로 설정함
+    /**
+     * 그룹을 생성하는 매핑입니다.
+     * 그룹의 이름과 열람날짜를 받아서 해당 그룹을 생성합니다.
+     * @param groupName : 생성할 그룹의 이름
+     * @param openDate : 생성할 그룹의 열람 날짜
+     * @return : 로그인한 메인 페이지로 이동
+     */
     @PostMapping("/group/new")
     public String create(HttpServletRequest request,
                          @RequestParam("groupName") String groupName,
@@ -44,12 +49,16 @@ public class GroupController {
         HttpSession session = request.getSession();
         Long memberId = Long.valueOf(String.valueOf(session.getAttribute("memberId")));
         groupService.createGroup(memberId, groupName, openDate);
-        return "redirect:/";
+        return "Main2";
     }
 
-    //Get 매핑으로 group/{id} 로 넘어오면 해당 그룹의 정보 데이터를 넘겨줌
-    //GroupMember 를 탐색해서 leader 를 찾고, leader 의 닉네임도 같이 넘겨준다.
-    //그룹의 정보를 보여주기 위한 info 페이지 필요
+    /**
+     * 해당 그룹에 대한 정보만을 반환하는 매핑입니다.
+     * 한 그룹을 선택했을때 모델에 그룹이름, 열람날짜, 리더닉네임을 넘겨줍니다.
+     * @param groupId : 검색할 그룹의 ID 값
+     * @Model : groupName, openDate, leaderNickname
+     * @return : info 페이지로 이동
+     */
     @GetMapping("/group/{id}")
     public String findOne(@PathVariable("id") Long groupId, Model model) {
         Group group = groupService.findOne(groupId);
@@ -61,32 +70,54 @@ public class GroupController {
                 leader = gm.getMember();
             }
         }
-
-        model.addAttribute(group.getGroupName());
-        model.addAttribute(group.getOpenDate());
-        model.addAttribute(leader.getNickname());
-
-        return "/info.html";
+        model.addAttribute("groupname", group.getGroupName());
+        model.addAttribute("opendate",group.getOpenDate());
+        model.addAttribute("leadernickname", leader.getNickname());
+        return "info";
     }
 
-    //Post 매핑으로 group/{id} 로 넘어오면 해당 그룹의 열람 날짜를 수정함
-    //form 으로 넘어온 LocalDateTime 인 newOpenDate 로 열람 날짜를 변경함
+    /**
+     * 그룹의 열람날짜를 수정하기 위한 매핑입니다.
+     * @param groupId : 그룹의 id 값
+     * @param newOpenDate : 수정할 열람 날짜(새 날짜)
+     * @return : group 페이지로 이동
+     */
     @PostMapping("/group/{id}")
     public String updateOpenDate(@PathVariable("id") Long groupId,
                                  @RequestParam("openDate") LocalDateTime newOpenDate) {
         groupService.updateOpenDate(groupId, newOpenDate);
-        return "redirect:/" + groupId + ".html";
+        return "groupList";
     }
 
-    //Get 매핑으로 group 으로 넘어오면 그룹 리스트를 보여줌
-    //groupSearch 로 검색 데이터를 전달받는데, 초기에는 조건이 없기 떄문에 모든 그룹이 다 나옴
-    //그룹 리스트를 표시하기 위한 group/groupList 페이지 필요
+    /**
+     * 기본적인 그룹 리스트를 띄우는 매핑입니다.
+     * 들어오면 모든 그룹을 검색해서 값을 반환합니다.
+     * @Model : groups
+     * @return : groupList 페이지로 이동
+     */
     @GetMapping("/group")
-    public String groupList(@RequestParam("sel") String option,
-                            @RequestParam("searchText") String text,
-                            Model model) {
+    public String groupList(Model model) {
         GroupSearch groupSearch = new GroupSearch();
-        if(option.equals("groupname")){
+        List<Group> groups = groupService.findGroups(groupSearch);
+        model.addAttribute("groups", groups);
+
+        return "groupList";
+    }
+
+    /**
+     * 검색한 그룹 정보를 반환하는 매핑입니다.
+     * 검색된 결과를 그룹 리스트 형태로 model 에 담아줍니다.
+     * @param option : 선택된 옵션(그룹명 or 리더닉네임 중 선택)
+     * @param text : 선택된 옵션에 대한 검색 내용(그룹이름 or 리더닉네임)
+     * @model : groups
+     * @return : groupList 페이지로 이동
+     */
+    @PostMapping("/groupList")
+    public String groupSearchList(@RequestParam("sel") String option,
+                                  @RequestParam("searchText") String text,
+                                  Model model) {
+        GroupSearch groupSearch = new GroupSearch();
+        if (option.equals("groupname")) {
             groupSearch.setGroupName(text);
         } else if (option.equals("leadername")) {
             groupSearch.setLeaderName(text);
@@ -94,14 +125,18 @@ public class GroupController {
         List<Group> groups = groupService.findGroups(groupSearch);
         model.addAttribute("groups", groups);
 
-        return "/groupList";
+        return "groupList";
     }
 
-    //Post 매핑으로 group/{id}/cancel 로 넘어오면 그룹을 삭제함
-    //넘어온 {id} 값을 사용해서 group 을 찾아서 group 을 삭제함
+    /**
+     * 그룹을 삭제하는 매핑입니다.
+     * 그룹을 삭제하고 메인화면으로 이동합니다.
+     * @param groupId : 그룹의 id 값
+     * @return : 로그인 한 메인페이지로 이동
+     */
     @PostMapping("/group/{id}/cancel")
     public String cancelGroup(@PathVariable("id") Long groupId) {
         groupService.cancelGroup(groupId);
-        return "redirect:/";
+        return "Main2";
     }
 }
