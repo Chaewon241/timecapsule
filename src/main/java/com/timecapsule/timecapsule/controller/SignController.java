1c1ee1d0d07c428dc04d0e052b2e2105232b223b
ggeggrgg <ggeggrgg@naver.com>
2022-06-05 20:47:04 +0900

    Update Annotation sign

--- src/main/java/com/timecapsule/timecapsule/controller/SignController.java
+++ src/main/java/com/timecapsule/timecapsule/controller/SignController.java
@@ -6,30 +6,38 @@ import com.timecapsule.timecapsule.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
+import java.util.Random;
 
 @Controller
 @RequiredArgsConstructor
 public class SignController {
 
     private final MemberService memberService;
-
-    //Get 매핑으로 signIn에 접근하면 로그인 폼을 화면에 보여줍니다.
-    //로그인 정보를 받기 위한 createSignInForm 사이트가 필요합니다.
+    
+    /**
+     * 로그인 폼 띄우기
+     * 로그인을 하기 위한 페이지로 이동합니다.
+     * @return : 로그인 페이지(signInForm)
+     */
     @GetMapping("/signIn")
     public String createForm(Model model){
         model.addAttribute("form", new SignInForm());
         return "signInForm";
     }
 
-    //Post 매핑으로 signIn으로 넘어오면 폼의 데이터들로 로그인을 진행합니다.
+    /**
+     * 로그인
+     * 직접적으로 로그인을 진행하는 매핑입니다.
+     * @param email : 로그인 이메일
+     * @param password : 로그인 비밀번호
+     * @return : 로그인 후 메인페이지
+     */
     @PostMapping("/signIn")
     public String signIn(HttpServletRequest request,
                          @RequestParam("email") String email,
@@ -45,14 +53,19 @@ public class SignController {
         return "Main2";
     }
 
-    //Get 매핑으로 signOut으로 접근하면 로그아웃을 진행합니다.
-    //세션에 저장된 속성 값을 지우고, 세션을 초기화합니다.
+    /**
+     * 로그아웃
+     * 로그인을 한 상태에서 로그아웃을 누르면 로그아웃을 진행합니다.
+     * 현재 session 에 저장된 memberId 값과 SIGNIN 값을 지우고
+     * 세션을 초기화합니다.
+     * @return : 로그인 전 메인페이지
+     */
     @GetMapping("/signOut")
     public String signOut(HttpServletRequest request){
         HttpSession session = request.getSession();
         session.removeAttribute("memberId");
         session.removeAttribute("SIGNIN");
         session.invalidate();
-        return "redirect:/";
+        return "Main";
     }
 }
